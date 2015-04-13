c5f26bc7e0ecf000b38b56e454578f5e4f4b7175
Gozz Kesshell <gozz.kesshell@gmail.com>
2015-04-13 18:24:05 +0300

    Add Quicksort
    Add Quicksort path with ready Quicksort programm an input.c file

---
+++ Quicksort/input.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+ void randInput(int *,int );
+ void bestInput(int *,int );
+ void worstInput(int *,int );
+
+
+void randInput(int *a, int n)
+{
+    int i;
+    srand(time(NULL));
+    for(i=0;i<n;i++)
+    {
+        a[i]=rand()%n;
+    }
+}
+
+void bestInput(int *a, int n)
+{
+    int i;
+    a[0]=0;
+    for(i=0;i<n;i++)
+    {
+        a[i]+=i;
+        if(a[i] == 21)
+            a[i]=348;
+    }
+
+}
+
+void worstInput(int *a, int n)
+{
+    int i;
+    a[0]=0;
+    for(i=0;i<n;i++)
+    {
+        a[i]=n-i;
+        if(a[i] == 21)
+            a[i]=348;
+    }
+}
---
+++ Quicksort/main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+ void QuickSort (int *a, int n);
+ void randInput(int *,int );
+ void bestInput(int *,int );
+ void worstInput(int *,int );
+
+
+
+void QuickSort (int *a, int n) {
+    int i, j, p, t;
+    if (n < 2)
+        return;
+    p = a[n / 2];
+    for (i = 0, j = n - 1;; i++, j--) {
+        while (a[i] < p)
+            i++;
+        while (p < a[j])
+            j--;
+        if (i >= j)
+            break;
+        t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+    QuickSort(a, i);
+    QuickSort(a + i, n - i);
+}
+
+int main () {
+    int *a;
+    int n;
+    int i;
+    int c=0;
+    printf("Input the number of numbers\n");
+    scanf("\n%d", &n);
+    a=(int *)malloc(n*sizeof(int));
+    printf("Choose how to full unsorted massive: 1 - rand,2 - best,3 - worst\n");
+    scanf("%d",&i);
+        switch(i)
+        {
+            case 1: randInput(a,n);
+            break;
+            case 2: bestInput(a,n);
+            break;
+            case 3: worstInput(a,n);
+            break;
+            default: exit(EXIT_FAILURE);
+        }
+    //int n = sizeof a / sizeof a[0];
+    for (i = 0; i < n; i++)
+    {   c+=20;
+        printf("%d%s", a[i], i == c - 1 ? "\n" : " ");}
+    QuickSort(a, n);
+    for (i = 0; i < n; i++)
+    {   c+=20;
+        printf("%d%s", a[i], i == c - 1 ? "\n" : " ");}
+    return 0;
+}
+
+
